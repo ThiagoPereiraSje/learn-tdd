f09b27ad7c90f0d0980d420c8e0e92a46d684260
Thiago Pereira <thiagopereiramail@gmail.com>
2021-12-12 20:12:43 +0000

    Mapping the complexity of ordering by selection

--- basics/src/selectionSort.js
+++ basics/src/selectionSort.js
@@ -1,13 +1,18 @@
 export function selectionSort(list) {
+  // N * (N + 3)
   for (let i = 0; i < list.length; i++) {
     let posMin = i;
 
+    // N * 1 * 1 = N
     for (let j = i; j < list.length; j++) {
+      // 1
       if (list[j] < list[posMin]) {
+        // 1
         posMin = j;
       }
     }
 
+    // 1 * (3) = 3
     if (list[i] > list[posMin]) {
       const aux = list[i];
       list[i] = list[posMin];
