b821ccfaf50bff78cea4d4241d59aeefbf714bcf
Thiago Pereira <thiagopereiramail@gmail.com>
2021-12-13 14:06:34 +0000

    Mapping the complexity of algorithms

--- basics/src/bubbleSort.js
+++ basics/src/bubbleSort.js
@@ -1,9 +1,9 @@
 export function bubbleSort(list) {
-  // (N -1) * 3(N -1)
+  // (N -1) * (4N -4)
   for (let i = 0; i < list.length - 1; i++) {
-    // (N -1) * 3
+    // (N -1) * 4 = 4N -4
     for (let j = 0; j < list.length - 1; j++) {
-      // 1 * 3 = 3
+      // 1 + 3 = 4
       if (list[j] > list[j + 1]) {
         const aux = list[j];
         list[j] = list[j + 1];
--- basics/src/selectionSort.js
+++ basics/src/selectionSort.js
@@ -1,18 +1,18 @@
 export function selectionSort(list) {
-  // (N - 1) * (N + 3)
+  // (N - 1) * (2N + 4)
   for (let i = 0; i < list.length - 1; i++) {
     let posMin = i;
 
-    // N * 1 * 1 = N
+    // N * 2 = 2N
     for (let j = i; j < list.length; j++) {
-      // 1
+      // 1 + 1 = 2
       if (list[j] < list[posMin]) {
         // 1
         posMin = j;
       }
     }
 
-    // 1 * (3) = 3
+    // 1 + 3 = 4
     if (list[i] > list[posMin]) {
       const aux = list[i];
       list[i] = list[posMin];
